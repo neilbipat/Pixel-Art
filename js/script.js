bbc7c41f47be8a0ce2a9aaf8394d154cecfd1533
Neil BipatE <neilbipat@gmail.com>
2019-09-05 21:29:05 -0400

    3 4 and 5

--- js/script.js
+++ js/script.js
@@ -5,16 +5,25 @@
 function crayon(){
   event.preventDefault();
   const color = document.getElementById('color-field').value;
-    document.querySelector('.brush').style.background = color
+    document.querySelector('.brush').style.background = color;
 
 };
 
 document.getElementById('set-color').addEventListener('click', crayon);
 
-//  commit 3
+//  commit 3 & 4
+
+// commit 5
+function change() {
+  this.style.background = document.getElementById('color-field').value;
+
+}
 
 for (let i = 0; i < 20; i++) {
   let div = document.createElement('div');
   div.setAttribute('class', 'square');
+  div.addEventListener('click' , change);
   document.body.appendChild(div);
-}
+};
+
+//
